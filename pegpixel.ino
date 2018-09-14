dc63b0e8874825f05a951260fbeb8f36796f4c04
Michael Dörsam <m.doersam@mailbox.org>
2018-09-14 17:34:38 +0200

    Convert incoming array-indices to pixel-index

--- pegpixel.ino
+++ pegpixel.ino
@@ -7,7 +7,7 @@
 
 #define NEOPIXEL_PIN            6
 
-#define NUMPIXELS      7
+#define NUMPIXELS      16
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -57,15 +57,35 @@ JsonObject& parseJson(String newMessage){
 }
 
 void drawPixel(JsonObject& json){
-  int y = json["y"];
-  y--; // The incoming array starts at 1. We need 0-based indices
-
+  int pixelIndex = getPixelIndex(json);
   if(json["s"] == "t"){
-    pixels.setPixelColor(y, pixels.Color(51, 102, 255));
+    pixels.setPixelColor(pixelIndex, pixels.Color(51, 102, 255));
    
   } else {
-    pixels.setPixelColor(y, pixels.Color(0, 0, 0));  
+    pixels.setPixelColor(pixelIndex, pixels.Color(0, 0, 0));  
     
   }
   pixels.show();
+}
+
+int getPixelIndex(JsonObject& json) {
+  int x = json["x"];
+  int y = json["y"];
+  
+  // convert 1-based indices to 0-based
+  x--;y--;
+  
+  if(x % 2 == 0) {
+    int pixelIndex = x * 4 + y;
+    Serial.println("line 0 or 2");
+    
+    Serial.print("pixelindex: ");
+    Serial.println(pixelIndex);
+    return pixelIndex;
+  }
+  int pixelIndex = x * 4 + (4 - (y+1));
+  Serial.print("pixelindex:");
+  Serial.println(pixelIndex);
+  return pixelIndex;
+
 }
