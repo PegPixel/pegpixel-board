b0388f4e71b687cd01ae51aa1bf8aec17ed586e5
Michael Dörsam <m.doersam@mailbox.org>
2018-09-14 16:51:00 +0200

    Replace usage of char-array with string

--- pegpixel.ino
+++ pegpixel.ino
@@ -5,19 +5,11 @@
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            6
+#define NEOPIXEL_PIN            6
 
-// How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      7
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 #define rxPin 10
 #define txPin 11
@@ -25,12 +17,6 @@ int delayval = 500; // delay for half a second
 SoftwareSerial mySerial(rxPin, txPin);
 
 void setup() {
-    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   int baudRate = 19200;
@@ -47,28 +33,18 @@ void setup() {
 
 void loop() { 
   if(mySerial.available()){
-    boolean newMessage = readCharArrayFromBluetooth();
-    if(newMessage == true){
-      JsonObject& json = parseJson();
-      drawPixel(json);
-    }
+    String newMessage = mySerial.readStringUntil('\n');
+    JsonObject& json = parseJson(newMessage);
+    drawPixel(json);
   }
 }
 
-const int readStringSize = 64;
-char readString[readStringSize] = "";
 
-const int minimumMessageSize = 21; 
-boolean readCharArrayFromBluetooth() {
-  int numberOfReadBytes = mySerial.readBytesUntil('\n', readString, readStringSize);
-  return numberOfReadBytes >= minimumMessageSize;
-}
-
-JsonObject& parseJson(){
+JsonObject& parseJson(String newMessage){
   const size_t bufferSize = JSON_OBJECT_SIZE(1) + 10;
   DynamicJsonBuffer jsonBuffer(bufferSize);
   
-  JsonObject& root = jsonBuffer.parseObject(readString);
+  JsonObject& root = jsonBuffer.parseObject(newMessage.c_str());
 
   if (!root.success()) {
     Serial.print("parseObject() failed - ");
@@ -77,13 +53,12 @@ JsonObject& parseJson(){
     root.printTo(Serial);
     Serial.print("\n");
   }
-  readString[readStringSize] = "";
   return root;
 }
 
 void drawPixel(JsonObject& json){
   int y = json["y"];
-  y--; // The incoming array starts at 1. We need 0
+  y--; // The incoming array starts at 1. We need 0-based indices
 
   if(json["s"] == "t"){
     pixels.setPixelColor(y, pixels.Color(51, 102, 255));
@@ -92,6 +67,5 @@ void drawPixel(JsonObject& json){
     pixels.setPixelColor(y, pixels.Color(0, 0, 0));  
     
   }
-  
   pixels.show();
 }
