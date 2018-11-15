4383e0a5c06afb56d4706fd623540005b309992a
SilasG <silas@graffy.info>
2018-11-15 12:07:21 +0100

    add hallsensor.ino as an example to read magnetic field from A1454

---
+++ hallsensor.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+const int SENSOR_ADR=0x60;
+const int HALL_REGISTER_ADR=0x1F;
+const int HALL_REGISTER_SIZE_BYTE=4;
+
+void setup() {
+  Wire.begin();       
+  Serial.begin(9600); 
+}
+
+void loop() {
+  Wire.beginTransmission(SENSOR_ADR);
+  Wire.write(HALL_REGISTER_ADR);
+  Wire.endTransmission(false);
+  Wire.requestFrom(SENSOR_ADR, HALL_REGISTER_SIZE_BYTE, true);  
+  
+  while (Wire.available()) { 
+    int i = Wire.read();
+    Serial.print(i);
+  }
+  Serial.println(" fertig ");
+  delay(500);
+}
